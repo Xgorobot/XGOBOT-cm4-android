31f14949ce9ec66d9d5a2e7f483cbdfd2de73b8b
zhangyibin <zhangyibin@weshare.com>
2022-11-06 17:19:21 +0800

    zhang_commit:运动模式修改

--- XgoApp/src/main/java/com/luwu/xgo_robot/mActivity/control/ControlActivity.java
+++ XgoApp/src/main/java/com/luwu/xgo_robot/mActivity/control/ControlActivity.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import com.blankj.utilcode.util.FragmentUtils;
 import com.luwu.xgo_robot.R;
 import com.luwu.xgo_robot.mActivity.control.fragment.AdvanceedFragment;
+import com.luwu.xgo_robot.mActivity.control.fragment.MotionFragment;
 import com.luwu.xgo_robot.mActivity.control.fragment.NormalFragment;
 import com.luwu.xgo_robot.mActivity.control.fragment.SingleLegFragment;
 import com.luwu.xgo_robot.mActivity.control.fragment.XYZFragment;
@@ -21,8 +22,8 @@ import com.luwu.xgo_robot.mActivity.control.fragment.XYZFragment;
  * <p>创建时间：2022/9/08<p>
  */
 public class ControlActivity extends AppCompatActivity {
-    private Fragment advancedFragment,normalFramgent,singlegFragment,xyzFragment;
-    private TextView mNormal_tv,mAdvance_tv,mSingleg_tv,mXYZ_tv;
+    private Fragment /*advancedFragment,normalFramgent,*/singlegFragment,xyzFragment,motionFragment;
+    private TextView mNormal_tv,mAdvance_tv,mSingleg_tv,mXYZ_tv,mMotion_tv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,38 +35,34 @@ public class ControlActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        FragmentUtils.hideAllShowFragment(normalFramgent);
-        mNormal_tv = findViewById(R.id.control_normal_tv);
+        FragmentUtils.hideAllShowFragment(singlegFragment);
+       /* mNormal_tv = findViewById(R.id.control_normal_tv);
         mNormal_tv.setOnClickListener(v -> {
             FragmentUtils.hideAllShowFragment(normalFramgent);
         });
         mAdvance_tv = findViewById(R.id.control_advance_tv);
         mAdvance_tv.setOnClickListener(v -> {
             FragmentUtils.hideAllShowFragment(advancedFragment);
-        });
+        });*/
         mSingleg_tv = findViewById(R.id.control_singleg_tv);
-        mSingleg_tv.setOnClickListener(v -> {
-            FragmentUtils.hideAllShowFragment(singlegFragment);
-        });
+        mSingleg_tv.setOnClickListener(v -> FragmentUtils.hideAllShowFragment(singlegFragment));
         mXYZ_tv = findViewById(R.id.control_xyz_tv);
-        mXYZ_tv.setOnClickListener(v -> {
-            FragmentUtils.hideAllShowFragment(xyzFragment);
-        });
+        mXYZ_tv.setOnClickListener(v -> FragmentUtils.hideAllShowFragment(xyzFragment));
+        mMotion_tv = findViewById(R.id.control_motion_tv);
+        mMotion_tv.setOnClickListener(v -> FragmentUtils.hideAllShowFragment(motionFragment));
     }
 
 
     private void initFragment() {
-        normalFramgent = new NormalFragment();
-        advancedFragment = new AdvanceedFragment();
         singlegFragment = new SingleLegFragment();
         xyzFragment = new XYZFragment();
+        motionFragment = new MotionFragment();
 
-        FragmentUtils.addFragment(getSupportFragmentManager(),normalFramgent,R.id.control_fragment,false);
-        FragmentUtils.addFragment(getSupportFragmentManager(),advancedFragment,R.id.control_fragment,false);
+
+        FragmentUtils.addFragment(getSupportFragmentManager(),motionFragment,R.id.control_fragment,false);
         FragmentUtils.addFragment(getSupportFragmentManager(),singlegFragment,R.id.control_fragment,false);
         FragmentUtils.addFragment(getSupportFragmentManager(),xyzFragment,R.id.control_fragment,false);
-        FragmentUtils.hideFragment(normalFramgent);
-        FragmentUtils.hideFragment(advancedFragment);
+        FragmentUtils.hideFragment(motionFragment);
         FragmentUtils.hideFragment(singlegFragment);
         FragmentUtils.hideFragment(xyzFragment);
     }
