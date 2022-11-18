0575c8dc45fcfe17cc21008d4319b06f23cf92cd
WenlinYuan <wenlin.yuan@ctechm.com>
2022-11-18 16:40:59 +0800

    [update] update view size

--- XgoApp/src/main/java/com/luwu/xgo_robot/data/RobotFunction.java
+++ XgoApp/src/main/java/com/luwu/xgo_robot/data/RobotFunction.java
@@ -3,6 +3,8 @@ package com.luwu.xgo_robot.data;
 import android.util.Log;
 import android.webkit.WebView;
 
+import com.luwu.xgo_robot.socket.SocketManager;
+
 import java.lang.ref.WeakReference;
 
 public class RobotFunction {
@@ -13,17 +15,25 @@ public class RobotFunction {
     * */
     public static void forward(int speed){
         Log.d(TAG, "forward: ");
+        String str = String.format("{\"func\":\"forward\",\"speed\":%s}",speed);
+        SocketManager.getInstance().write(str);
     }
 
     public static void backward(int speed){
         Log.d(TAG, "backward: ");
+        String str = String.format("{\"func\":\"backward\",\"speed\":%s}",speed);
+        SocketManager.getInstance().write(str);
     }
     public static void left(int speed){
         Log.d(TAG, "left: ");
+        String str = String.format("{\"func\":\"left\",\"speed\":%s}",speed);
+        SocketManager.getInstance().write(str);
     }
 
     public static void right(int speed){
         Log.d(TAG, "right: ");
+        String str = String.format("{\"func\":\"right\",\"speed\":%s}",speed);
+        SocketManager.getInstance().write(str);
     }
 
     public static String getWebUrl(String cameraUrl){
